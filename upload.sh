186184f6a18a26d585998bc4546e1fc7574f7e63
xitiomet <brian@openstatic.org>
2026-03-12 02:14:42 -0400

    added upload script

---
+++ upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+scp target/rms-cast-recorder-1.0-native* openstatic.org:openstatic.org/projects/rms-cast-recorder/
+scp target/rms-cast-recorder-1.0-java.deb openstatic.org:openstatic.org/projects/rms-cast-recorder/
+scp target/rms-cast-recorder-1.0.jar openstatic.org:openstatic.org/projects/rms-cast-recorder/
+scp README.md openstatic.org:openstatic.org/projects/rms-cast-recorder/
