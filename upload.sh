79e0e973b973f8031b8094868946b433dd5d2cd4
xitiomet <brian@openstatic.org>
2026-05-23 10:46:04 -0400

    update upload script

--- upload.sh
+++ upload.sh
@@ -2,5 +2,5 @@
 scp target/radio-pipe-1.0-native* openstatic.org:openstatic.org/projects/radio-pipe/
 scp target/radio-pipe-1.0-java.deb openstatic.org:openstatic.org/projects/radio-pipe/
 scp target/radio-pipe-1.0.jar openstatic.org:openstatic.org/projects/radio-pipe/
-scp target/radio-pipe-1.0.exe openstatic.org:openstatic.org/projects/radio-pipe/
+scp target/radio-pipe-setup.exe openstatic.org:openstatic.org/projects/radio-pipe/
 scp README.md openstatic.org:openstatic.org/projects/radio-pipe/
